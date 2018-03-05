93bab85d6165a7b95bb1eed14e6311a42b3593ed
Vera Onunda <veonun@ttu.ee>
2018-03-05 18:26:11 +0200

    improve alert message for contact info sent and remove console logs

--- js/main.js
+++ js/main.js
@@ -11,17 +11,18 @@ for (var i = 0; i < k.length; i++) {
         this.style["background-color"] = '#383E4C';
     };
 }
-/*Display all information when click send*/
 
+/*Display all information when click send*/
 /*Check for empty form fields and alert if empty*/
 var form = document.querySelector('form.contact-form');
 form.onsubmit=function () {
     var field = document.getElementsByTagName("input");
+    var result = new Array();
     for (var i = 0; i < field.length; i++) {
         if (field[i].value == "") {
             alert('Please fill in the empty' + ' ' + field[i].getAttribute("name") + ' ' + 'field');
         } else {
-            alert(field[i].getAttribute("name")+" Value sent is"+field[i].value);
+            result.push(field[i].getAttribute("name")+":"+field[i].value);
         }
     }
 
@@ -29,17 +30,16 @@ form.onsubmit=function () {
     if (textComment.value == "") {
         alert('Please fill empty' + ' ' + textComment.getAttribute("name") + ' ' + 'field');
     }else{
-        alert(textComment.getAttribute("name")+'Value sent is '+textComment.value);
+        result.push(textComment.getAttribute("name")+':'+textComment.value);
     }
+    alert(result.join('\n'));
 }
 
 /*On click of the green button, change the text of the button to a different text and the color of the button to a different colour
  <a href="#" class="feature-content-link green-btn">button green</a>`*/
 var buttonGreen = document.getElementsByClassName("feature-content-link green-btn")[0];
     buttonGreen.onclick = function (){
-        console.log(buttonGreen.innerHTML="Change");
         buttonGreen.innerHTML="Yippee"
-        console.log(buttonGreen.className="feature-content-link red-btn");
         buttonGreen.className="feature-content-link red-btn"
 }
 
@@ -47,7 +47,6 @@ var buttonGreen = document.getElementsByClassName("feature-content-link green-bt
 * <li><a href="http://www.facebook.com/templatemo" class="external" target="_blank">External</a></li>*/
 var fb =document.getElementsByClassName("external")[0];
    fb.onclick = function(){
-       console.log(fb.href);
-       fb.href="https://www.facebook.com/pg/worldrugby7s";
+   fb.href="https://www.facebook.com/pg/worldrugby7s";
 }
 
